8fe2fdc831815d62ef40ab4d6bb618a82328b25f
Sarbojit Das <sarbojitdas70@gmail.com>
2026-07-02 21:07:10 +0530

    Time: 0 ms (100%), Space: 14.8 MB (30.96%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(int i : nums1){
+            m[i]++;
+        }
+        for(int i : nums2){
+            if(m[i]>0){
+                ans.push_back(i);
+                m[i]--;
+            }
+        }
+        return ans;
+    }
+};
