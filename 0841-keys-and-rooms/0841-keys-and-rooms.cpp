2b2f35619d2498d92880fa40060fc1a55921f468
Sarbojit Das <sarbojitdas70@gmail.com>
2026-07-05 12:48:38 +0530

    Time: 0 ms (100%), Space: 14.5 MB (72.77%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //Using BFS
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> visited(n,false);
+        
+        queue<int> q;
+        q.push(0);
+        visited[0]=true;
+
+        while(!q.empty()){
+            int room = q.front();
+            q.pop();
+
+            for(int key : rooms[room]){
+                if(!visited[key]){
+                    visited[key]=true;
+                    q.push(key);
+                }
+            }
+        }
+        for(bool room : visited) {
+            if(!room) return false;
+        }
+        return true;
+
+    }
+};
