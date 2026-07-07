54b9d0d237a316435fc084aba3e7d6b90d0b456e
Sarbojit Das <sarbojitdas70@gmail.com>
2026-07-07 19:50:52 +0530

    Time: 3 ms (65.8%), Space: 8.2 MB (16.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int fibDP(int n , vector<int> &f){
-        if(n==0||n==1) return n;
-        if(f[n]!=-1) return f[n];
-        f[n]=fibDP(n-1,f)+fibDP(n-2,f);
-        return f[n];
-    }
     int fib(int n) {
-        vector<int> f(n+1,-1);
-        return fibDP(n,f);
+        if(n==0 || n==1) return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
