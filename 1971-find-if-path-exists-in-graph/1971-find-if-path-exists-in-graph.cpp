5be2f0793901656484214fb3ddbbed3428180f08
Sarbojit Das <sarbojitdas70@gmail.com>
2026-09-04 18:46:21 +0530

    Time: 390 ms (38.58%), Space: 315.6 MB (34.68%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        // Build adjacency list
         vector<vector<int>> adj(n);
-
-        for (auto edge : edges) {
+        for(auto edge: edges){
             int u = edge[0];
             int v = edge[1];
-
             adj[u].push_back(v);
-            adj[v].push_back(u);  // undirected graph
+            adj[v].push_back(u);
         }
-
-
-        vector<int> visited(n,0) ;
-        visited[source] = 1;
+        vector<int> visited(n,0);
         queue<int> q;
         q.push(source);
+        visited[source]=1;
+
         while(!q.empty()){
             int currNode = q.front();
             q.pop();
-            if(currNode==destination) return true;
-            for(auto it : adj[currNode]){
+            if(currNode == destination) return true;
+            for(auto it: adj[currNode]){
                 if(!visited[it]){
-                    visited[it] = 1;
+                    visited[it]=1;
                     q.push(it);
                 }
             }
-
         }
         return false;
+
     }
 };
